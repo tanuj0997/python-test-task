dc7c6dfe990d2396c5c903697f4d881c3fb44637
Tanuj <tanuj@gmail.com>
2020-04-08 01:08:13 +0530

    feat: Added csvReader API with ndays filter

--- csvReader/views.py
+++ csvReader/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import csv,os,json,datetime
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from collections import OrderedDict
 
-# Create your views here.
+@api_view()
+def csvReader(request):
+    try:
+        requestData = dict(request.GET)
+        ndays = None
+        print(requestData['ndays'])
+        if not requestData['ndays'] or requestData['ndays'][0] == '' or int(requestData['ndays'][0]) <= 0:
+            ndays = datetime.datetime.today() - datetime.timedelta(days=7)
+        else:
+            ndays = datetime.datetime.today() - datetime.timedelta(days=int(requestData['ndays'][0]))
+
+        csvFile = os.path.relpath('./data/data.csv')
+        with open(csvFile,newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            csvData = []
+            for row in reader:
+                row = dict(OrderedDict(row))
+
+                if ndays < datetime.datetime.strptime(row['date'],'%d-%m-%y'):
+                    csvData.append(row)
+
+        return Response({"data":csvData})
+        
+    except (Exception,TypeError,ValueError) as error:
+        return Response({
+            'error' : str(error),
+        })
+        
